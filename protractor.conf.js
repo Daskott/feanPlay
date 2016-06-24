8cf3c483b31130b9e40d037b76a7ae00a8dfbe44
daskott <cottee2@localhost.localdomain>
2016-06-23 23:26:26 -0500

    cleanup v1.2

--- protractor.conf.js
+++
@@ -1,9 +0,0 @@
-exports.config = {
- framework: 'mocha', 
- specs: [ 'test/e2e/**/*.spec.js' ],
-//  capabilities: {
-//   browserName: 'phantomjs',
-//   'phantomjs.binary.path': require('phantomjs').path
-// }, 
- mochaOpts: { enableTimeouts: false } 
-};
